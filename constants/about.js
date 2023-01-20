9172f20b51865700727b594c17e64f81c459e69e
mariyamateo <ma.clarissepmateo@gmail.com>
2023-01-21 01:08:48 +0800

    added about list in about page

---
+++ constants/about.js
@@ -0,0 +1,40 @@
+export const ABOUT_LIST = [
+  {
+    id: 0,
+    about: "1 year of experience in front-end development",
+  },
+  {
+    id: 1,
+    about: "Solid understanding of HTML, CSS, and JavaScript",
+  },
+  {
+    id: 2,
+    about: "Experience working with popular front-end frameworks such as React",
+  },
+  {
+    id: 3,
+    about: "Strong attention to detail and problem-solving skills",
+  },
+  {
+    id: 4,
+    about: "Ability to work well in a team",
+  },
+  {
+    id: 5,
+    about: "Experience in building responsive and user-friendly interfaces",
+  },
+  {
+    id: 6,
+    about: "Passion for creating visually appealing and engaging websites",
+  },
+  {
+    id: 7,
+    about:
+      "Motivated to continue growing skills and experience in a professional setting",
+  },
+  {
+    id: 8,
+    about:
+      "Interested in contributing to the success of a front-end development team",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
       <Navbar/>
       <Hero />
       <About />
+      <Hero />
     </div>
   );
 }

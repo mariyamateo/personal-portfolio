f12949aa624b97f233d19ec0225b30a092e4bc1a
Ma. Clarisse Mateo <ma.clarissemateo@MacBook-Pro.local>
2023-02-03 15:40:32 +0800

    add marquee

---
+++ components/Marquee/Marquee.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { SKILLS } from "@/constants/marquee-skills";
+import Marquee from "react-fast-marquee";
+
+const MarqueeSkills = () => {
+  return (
+    <div className="-rotate-2 bg-white">
+      <Marquee gradient={false} speed={100}>
+        <div className="flex flex-row items-center py-5">
+          {SKILLS.map((skill, index) => (
+            <div className="flex flex-row items-center">
+              <h6 className="px-4 text-black">{skill.name}</h6>
+              <h6 className="text-[#FF8E5E]">*</h6>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row items-center py-5">
+          {SKILLS.map((skill, index) => (
+            <div className="flex flex-row items-center">
+              <h6 className="px-4 text-black">{skill.name}</h6>
+              <h6 className="text-[#FF8E5E]">*</h6>
+            </div>
+          ))}
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueeSkills;
---
+++ constants/marquee-skills.js
@@ -0,0 +1,62 @@
+export const SKILLS = [
+  {
+    id: 0,
+    name: "HTML5",
+  },
+  {
+    id: 1,
+    name: "CSS3",
+  },
+  {
+    id: 2,
+    name: "JAVASCRIPT",
+  },
+  {
+    id: 3,
+    name: "TYPESCRIPT",
+  },
+  {
+    id: 4,
+    name: "FRONTEND",
+  },
+  {
+    id: 5,
+    name: "FIGMA",
+  },
+  {
+    id: 6,
+    name: "UI/UX",
+  },
+  {
+    id: 7,
+    name: "GITHUB",
+  },
+  {
+    id: 8,
+    name: "SASS",
+  },
+  {
+    id: 9,
+    name: "REACT",
+  },
+  {
+    id: 10,
+    name: "NEXTJS",
+  },
+  {
+    id: 11,
+    name: "NODEJS",
+  },
+  {
+    id: 12,
+    name: "VUEJS",
+  },
+  {
+    id: 13,
+    name: "WEB3",
+  },
+  {
+    id: 14,
+    name: "DEVELOPER",
+  },
+];
